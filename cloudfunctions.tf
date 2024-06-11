8a84b93f7bbed5fac62e59dd5caf3104218bc3e9
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-10 20:21:03 -0400

    Update cloudfunctions.tf

--- cloudfunctions.tf
+++ cloudfunctions.tf
@@ -1,20 +1,20 @@
 locals {
-  project = "george_ibrahim" # Google Cloud Platform Project ID
+  project1 = "george_ibrahim" # Google Cloud Platform Project ID
 }
 
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "bucket1" {
   name     = "${local.project}-gcf-source"  # Every bucket name must be globally unique
   location = "US"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_object" "object" {
+resource "google_storage_bucket_object" "object1" {
   name   = "function-source.zip"
   bucket = google_storage_bucket.bucket.name
   source = "function-source.zip"  # Add path to the zipped function source code
 }
 
-resource "google_cloudfunctions2_function" "function" {
+resource "google_cloudfunctions2_function" "function1" {
   name = "function-v2"
   location = "us-central1"
   description = "a new function"
