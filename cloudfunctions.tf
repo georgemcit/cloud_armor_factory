73f2fe16dcc225d403cdfa54da798736e733ceec
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-10 20:09:38 -0400

    Update cloudfunctions.tf

--- cloudfunctions.tf
+++ cloudfunctions.tf
@@ -36,3 +36,146 @@ resource "google_cloudfunctions2_function" "function" {
     timeout_seconds     = 60
   }
 }
+locals {
+  project = "george_ibrahim1" # Google Cloud Platform Project ID
+}
+
+resource "google_service_account" "account" {
+  account_id = "gcf-sa"
+  display_name = "Test Service Account"
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = "functions2-topic"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "${local.project}-gcf-source"  # Every bucket name must be globally unique
+  location = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "function-source.zip"  # Add path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "function" {
+  name = "gcf-function"
+  location = "us-central1"
+  description = "a new function"
+
+  build_config {
+    runtime = "nodejs16"
+    entry_point = "helloPubSub"  # Set the entry point 
+    environment_variables = {
+        BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count  = 3
+    min_instance_count = 1
+    available_memory    = "4Gi"
+    timeout_seconds     = 60
+    max_instance_request_concurrency = 80
+    available_cpu = "4"
+    environment_variables = {
+        SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email = google_service_account.account.email
+  }
+
+  event_trigger {
+    trigger_region = "us-central1"
+    event_type = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic = google_pubsub_topic.topic.id
+    retry_policy = "RETRY_POLICY_RETRY"
+  }
+}
+locals {
+  project = "george_ibrahim2" # Google Cloud Platform Project ID
+}
+
+resource "google_service_account" "account" {
+  account_id   = "gcf-sa"
+  display_name = "Test Service Account"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "${local.project}-gcf-source"  # Every bucket name must be globally unique
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "function-source.zip"  # Add path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "function" {
+  name        = "gcf-function" # name should use kebab-case so generated Cloud Run service name will be the same
+  location    = "us-central1"
+  description = "a new function"
+
+  build_config {
+    runtime     = "nodejs16"
+    entry_point = "helloHttp"  # Set the entry point
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    min_instance_count    = 1
+    available_memory      = "256M"
+    timeout_seconds       = 60
+    service_account_email = google_service_account.account.email
+  }
+}
+
+resource "google_cloudfunctions2_function_iam_member" "invoker" {
+  project        = google_cloudfunctions2_function.function.project
+  location       = google_cloudfunctions2_function.function.location
+  cloud_function = google_cloudfunctions2_function.function.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_cloud_run_service_iam_member" "cloud_run_invoker" {
+  project  = google_cloudfunctions2_function.function.project
+  location = google_cloudfunctions2_function.function.location
+  service  = google_cloudfunctions2_function.function.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_cloud_scheduler_job" "invoke_cloud_function" {
+  name        = "invoke-gcf-function"
+  description = "Schedule the HTTPS trigger for cloud function"
+  schedule    = "0 0 * * *" # every day at midnight
+  project     = google_cloudfunctions2_function.function.project
+  region      = google_cloudfunctions2_function.function.location
+
+  http_target {
+    uri         = google_cloudfunctions2_function.function.service_config[0].uri
+    http_method = "POST"
+    oidc_token {
+      audience              = "${google_cloudfunctions2_function.function.service_config[0].uri}/"
+      service_account_email = google_service_account.account.email
+    }
+  }
+}
